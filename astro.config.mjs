e48ece6a1a41708ed532a90646f1d0223a7c8779
Jonathan Forrider <jonathanforrider@Jonathans-MBP.home>
2023-12-23 12:52:55 -0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: 'The BETH Stack',
+    social: {
+      github: 'https://github.com/ethanniser/the-beth-stack'
+    },
+    sidebar: [{
+      label: 'Start Here',
+      autogenerate: {
+        directory: 'start_here'
+      }
+    }, {
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  })],
+  output: "server",
+  adapter: vercel()
+});
